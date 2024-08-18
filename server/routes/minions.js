6c8e5791be8e96758070feb6caf3d757b42c87ed
Hadj H <36198012+elhadjx@users.noreply.github.com>
2024-08-18 12:02:31 +0100

    Add: Minions Route

--- server/routes/minions.js
+++ server/routes/minions.js
@@ -1,5 +1,46 @@
-const express = require('express')
+const express = require('express');
+const { getFromDatabaseById, getAllFromDatabase, addToDatabase, updateInstanceInDatabase, deleteFromDatabasebyId } = require('../db');
 const minionsRoute = express.Router();
 
+minionsRoute.param('minionId', (req, res, next, id) => {
+
+    const minion = getFromDatabaseById('minions', id)
+
+    if (minion) {
+        req.minion = minion;
+        next()
+    } else {
+        res.status(404).send({ error: 'Minion not found' })
+    }
+
+})
+
+minionsRoute.get('/', (req, res, next) => {
+
+    const allMinions = getAllFromDatabase('minions')
+    res.status(200).send(allMinions)
+
+})
+
+minionsRoute.post('/', (req, res, next) => {
+
+    const minion = req.body
+    addToDatabase('minions', minion)
+    res.sendStatus(204)
+})
+
+minionsRoute.get('/:minionId', (req, res, next) => {
+    res.status(200).send(req.minion)
+})
+
+minionsRoute.put('/:minionId', (req, res, next) => {
+    updateInstanceInDatabase('minions', req.body)
+    res.status(200).send(req.body)
+})
+
+minionsRoute.delete('/:minionId', (req, res, next) => {
+    deleteFromDatabasebyId(req.minion.id)
+    res.sendStatus(204);
+})
 
 module.exports = minionsRoute
